3d51ab2b43989e0baad40ea8939a4da688aa9690
Haris-199 <97857344+Haris-199@users.noreply.github.com>
2024-08-16 23:04:15 -0400

    Make form for information

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,29 @@
+import "../styles/form.css";
+
 export default function Resume() {
-    return (
-      <div className="form">
-        hello form
-        <h1>h1</h1>
-        <h2>h2</h2>
-      </div>
-    );
-  }
+  return (
+    <div className="form">
+      <form action="">
+        <div className="personal">
+          <h2>Personal Information</h2>
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" id="name" />
+
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" />
+
+          <label htmlFor="number">Phone Number</label>
+          <input type="tel" name="number" id="number" />
+        </div>
+
+        <div className="education">
+          <h2>Education</h2>
+        </div>
+
+        <div className="experience">
+          <h2>Experience</h2>
+        </div>
+      </form>
+    </div>
+  );
+}
