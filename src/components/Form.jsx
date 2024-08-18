08ffa2a93a4672a407893a1a21afc04f6cc2513b
Haris-199 <97857344+Haris-199@users.noreply.github.com>
2024-08-17 22:07:54 -0400

    Add personal info to state

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,50 @@
+import { useState } from "react";
 import "../styles/form.css";
 
 export default function Resume() {
+  const [personalInfo, setPersonalInfo] = useState({
+    name: "",
+    email: "",
+    number: "",
+  });
+
   return (
     <div className="form">
       <form action="">
         <div className="personal">
           <h2>Personal Information</h2>
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" id="name" />
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={personalInfo.name}
+            onChange={(e) =>
+              setPersonalInfo({ ...personalInfo, name: e.target.value })
+            }
+          />
 
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={personalInfo.email}
+            onChange={(e) =>
+              setPersonalInfo({ ...personalInfo, email: e.target.value })
+            }
+          />
 
           <label htmlFor="number">Phone Number</label>
-          <input type="tel" name="number" id="number" />
+          <input
+            type="tel"
+            name="number"
+            id="number"
+            value={personalInfo.number}
+            onChange={(e) =>
+              setPersonalInfo({ ...personalInfo, number: e.target.value })
+            }
+          />
         </div>
 
         <div className="education">
