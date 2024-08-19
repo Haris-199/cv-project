de8262b1f7b6e2761dd2e151eee0c06fd57b573d
Haris-199 <97857344+Haris-199@users.noreply.github.com>
2024-08-19 01:41:20 -0400

    Create App file

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Form from "./components/Form.jsx";
+import Resume from "./components/Resume.jsx";
+
+export default function App() {
+  return (
+    <>
+      <Form />
+      <Resume />
+    </>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Form from './components/Form.jsx'
-import Resume from './components/Resume.jsx'
+import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Form />
-    <Resume />
+    <App />
   </StrictMode>,
 )
