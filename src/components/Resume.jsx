b11fe2757a4c9e16574d8bd0b3cacc2642c02921
Haris-199 <97857344+Haris-199@users.noreply.github.com>
2024-08-23 04:12:55 -0400

    Display education on CV

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -7,10 +7,26 @@ export default function Resume({ info }) {
         <div className="banner">
           <p>{info.personal.name ? info.personal.name : String.fromCharCode(160)}</p>
         </div>
-        <div className="contact">
-            {info.personal.email && <p><span className="email-icon icon"></span>{info.personal.email}</p>}
-            {info.personal.number && <p><span className="number-icon icon"></span>{info.personal.number}</p>}
-            {info.personal.address && <p><span className="address-icon icon"></span>{info.personal.address}</p>}
+        <div className="text-margin">
+          { (info.personal.email || info.personal.number || info.personal.address) &&
+            <div className="contact">
+              {info.personal.email && <p><span className="email-icon icon"></span>{info.personal.email}</p>}
+              {info.personal.number && <p><span className="number-icon icon"></span>{info.personal.number}</p>}
+              {info.personal.address && <p><span className="address-icon icon"></span>{info.personal.address}</p>}
+            </div>
+          }
+
+          {info.education.length > 0 && <h2 className="seperator">Education</h2>}
+          <div className="education-list">
+            {info.education.map((obj) =>
+              <div key={obj.school} className="edu-item">
+                <p><strong>{obj.school}</strong></p>
+                <p>{obj.location}</p>
+                <p><strong>{obj.degree}</strong></p>
+                <p>{obj.start}-{obj.end}</p>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
