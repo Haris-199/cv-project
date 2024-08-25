e9b694a3aafe4b4e68492b583321a7fb513c9903
Haris-199 <97857344+Haris-199@users.noreply.github.com>
2024-08-25 04:20:05 -0400

    Clear sample data

--- src/App.jsx
+++ src/App.jsx
@@ -11,24 +11,7 @@ export default function App() {
       address: "",
     },
     education: [],
-    experience: [
-      {
-        title: "Batman",
-        company: "Wayne Enterprises",
-        location: "Gotham City",
-        start: "1900",
-        end: "1801",
-        description: ["Saved people.", "Helped peoples."],
-      },
-      {
-        title: "Spiderman",
-        company: "n/a",
-        location: "New York City",
-        start: "2020",
-        end: "2021",
-        description: ["Did a lot of cool hardwork such as saving people.", "Did stuff adjsksajlk kasjdlkasjdlk saouyd asudh  sajdh askj fehua hsdkhas kjd.", "Haris is cool."],
-      },
-    ],
+    experience: [],
   });
 
   return (
